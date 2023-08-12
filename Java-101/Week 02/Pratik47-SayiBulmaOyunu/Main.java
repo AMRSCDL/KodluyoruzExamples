6071810d4ed7f4767d29fb8db561091e131b34cf
Ahmet Emir Demirtaş <aedemirtas00@gmail.comn>
2023-08-12 19:03:07 +0300

    Java-101 Examples

---
+++ Java-101/Week 02/Pratik47-SayiBulmaOyunu/Main.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi = (int) (Math.random() * 100);
+        int tahmin;
+        int hak = 5;
+        boolean devam = true;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(sayi);
+        System.out.println("0-100 arası bir sayı arıyorsunuz");
+
+        do {
+            System.out.println("Tahmininizi giriniz:");
+            tahmin = input.nextInt();
+
+            if (tahmin < 0 || tahmin > 100) {
+                System.out.println("Hatalı giriş yaptınız. Tekrar deneyin");
+            } else {
+                if (tahmin == sayi) {
+                    System.out.println("Doğru tahmin.");
+                    devam = false;
+                } else if (hak <= 1) {
+                    devam = false;
+                } else {
+                    hak--;
+                    if (tahmin > sayi) {
+                        System.out.println("Girdiğiniz sayı gizli sayıdan daha büyük.");
+                    } else {
+                        System.out.println("Girdiğiniz sayı gizli sayıdan daha küçük.");
+                    }
+                    System.out.println("Tekrar deneyiniz. Kalan can: " + hak);
+                }
+            }
+        } while (devam);
+
+        System.out.println("Aradığınız sayı: " + sayi);
+        if (tahmin == sayi) {
+            System.out.println("Oyun bitti. Kazandınız!");
+        } else {
+            System.out.println("Oyun bitti. Kaybettiniz!");
+        }
+    }
+}
